5b3a4babb6ba6503f7a4c0190582ae00d6e5d200
swiftyy188 <jeffho.ml@gmail.com>
2015-12-16 15:37:13 -0500

    added empty functions

--- tt.js
+++ tt.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+	var winningCombos = [[1,2,3], [4,5,6], [7,8,9]]
 	var turn = 1;
 	
 	$('td').on('click', function(){
@@ -10,8 +11,13 @@ $(document).ready(function(){
 		}else{
 			$(this).html('O');
 		}
+		//check for winner
+		checkForWinner();
 			$(this).off('click');
 			turn ++;
 	});
+	function checkForWinner(){
+		//check
+	}
 
 });
